4f8102be1b0cd1b5b6765f8ba64d221297f97aed
Garrett Reichman <gsreichman@gmail.com>
2019-12-21 10:30:52 -0500

    added onclicks to answers

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+const questions = [
+
+{
+    question: "?",
+    answers: ["1"," 2"," 3","4 "],
+    correct: 1
+},
+{
+    question: "?",
+    answers: ["1"," 2"," 3","4 "],
+    correct: 2
+},
+{
+    question: "?",
+    answers: ["1"," 2"," 3","4 "],
+    correct: 3
+},
+{
+    question: "?",
+    answers: ["1"," 2"," 3","4 "],
+    correct: 4
+},
+{
+    question: "?",
+    answers: ["1"," 2"," 3","4 "],
+    correct: 5
+},
+
+
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
 function beginQuiz(){
 document.querySelector("#startButton").style.display="none";
 var answers= document.querySelectorAll(".answers");
+var currentQ;
 for(let i=0; i<answers.length; i++){
     answers[i].style.display="block";
+}//for
+getQuestion();
+}//begin quiz
+
+function chooseAnswer(){
+    var test = event.target.dataset.answer;
+    console.log(test);
+    if(event.target.tagName.toLowerCase()==='p'){
+    console.log("is it a paragraph? ");}
+//    console.log( event.target.dataset.answer);
 }
+
+function getQuestion(){
+    currentQ=questions[0];
+    console.log(currentQ.question);
+    console.log(currentQ.answers);
+    console.log(currentQ.correct);
 }
