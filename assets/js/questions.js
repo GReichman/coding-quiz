074e35a3205fd7e50b303bc620faef5786550b8b
Garrett Reichman <gsreichman@gmail.com>
2019-12-23 18:29:35 -0500

    added questions and ability to display the questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,28 +1,28 @@
 const questions = [
 
 {
-    question: "?",
-    answers: ["1"," 2"," 3","4 "],
-    correct: 1
-},
-{
-    question: "?",
-    answers: ["1"," 2"," 3","4 "],
-    correct: 2
-},
-{
-    question: "?",
-    answers: ["1"," 2"," 3","4 "],
+    question: "Commonly used data types DO NOT include: ",
+    answers: ["Strings","Booleans","Alerts","Numbers"],
     correct: 3
 },
 {
-    question: "?",
-    answers: ["1"," 2"," 3","4 "],
+    question: "The condition in an if / else statement is enclosed within ____.",
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correct: 3
+},
+{
+    question: "Arrays in JavaScript can be used to store _______",
+    answers: ["numbers and strings","other arrays","booleans","all of the above"],
     correct: 4
 },
 {
-    question: "?",
-    answers: ["1"," 2"," 3","4 "],
+    question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+    answers: ["Javascript","terminal/bash","for loops","console.log"],
+    correct: 4
+},
+{
+    question: "What is the worst part about CSS",
+    answers: ["floats","floats","floats","all of the above"],
     correct: 5
 },
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,45 @@
-function beginQuiz(){
-document.querySelector("#startButton").style.display="none";
-var answers= document.querySelectorAll(".answers");
-var currentQ;
-for(let i=0; i<answers.length; i++){
-    answers[i].style.display="block";
-}//for
-getQuestion();
+function beginQuiz() {
+    document.querySelector("#startButton").style.display = "none";
+    var answers = document.querySelectorAll(".answers");
+    var currentQ;
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].style.display = "block";
+    }//for
+    getQuestion();
 }//begin quiz
 
-function chooseAnswer(){
+function chooseAnswer() {
     var test = event.target.dataset.answer;
-    console.log(test);
-    if(event.target.tagName.toLowerCase()==='p'){
-    console.log("is it a paragraph? ");}
-//    console.log( event.target.dataset.answer);
+    if (event.target.tagName.toLowerCase() === 'p') {
+       if(test==currentQ.correct || currentQ.correct===5){
+           correctAnswer();
+           console.log("correct answer");
+       }
+       else{
+           wrongAnswer();
+           console.log("wrong answer");
+       }
+    }
 }
 
-function getQuestion(){
-    currentQ=questions[0];
+function getQuestion() {
+    currentQ = questions[4];
     console.log(currentQ.question);
     console.log(currentQ.answers);
     console.log(currentQ.correct);
+    $("#questionHeader").html(currentQ.question);
+    $("#answer1").html("1. " + currentQ.answers[0]);
+    $("#answer2").html("2. " + currentQ.answers[1]);
+    $("#answer3").html("3. " + currentQ.answers[2]);
+    $("#answer4").html("4. " + currentQ.answers[3]);
 }
+
+function wrongAnswer(){
+
+}//wrong
+
+function correctAnswer(){
+
+}//right
+
+
